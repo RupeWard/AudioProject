1e10999472cabf3d3465cedfd9c7debba542728c
RupeWard <rupe@rupertward.co.uk>
2017-05-01 21:49:33 +0100

    GraphScrollBar WIP - ScrollBar and ScrollBarEnds, with latter instantiated from prefabs - plus various textures

---
+++ AudioProjectUnity/Assets/Scripts/Display/GraphScrollBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RJWS.Graph
+{
+	public class GraphScrollBar : MonoBehaviour
+	{
+		private Dictionary<GraphScrollBarEnd.EEnd, GraphScrollBarEnd> _ends = new Dictionary<GraphScrollBarEnd.EEnd, GraphScrollBarEnd>( );
+		public RectTransform cachedRT
+		{
+			private set;
+			get;
+		}
+
+		private void Awake()
+		{
+			cachedRT = GetComponent<RectTransform>( );
+			Init( );
+		}
+
+		public void Init()
+		{
+			GameObject endPrefab = Resources.Load<GameObject>( "Graph/Prefabs/ScrollBarEnd" );
+
+			foreach (GraphScrollBarEnd.EEnd eend in System.Enum.GetValues( typeof( GraphScrollBarEnd.EEnd ) ))
+			{
+				GameObject go = GameObject.Instantiate( endPrefab );
+				_ends[eend] = go.GetComponent<GraphScrollBarEnd>( );
+				_ends[eend].Init( this, eend );
+			}
+		}
+	}
+
+}
---
+++ AudioProjectUnity/Assets/Scripts/Display/GraphScrollBarEnd.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RJWS.Graph
+{
+	public class GraphScrollBarEnd : MonoBehaviour
+	{
+		public enum EEnd
+		{
+			Min,
+			Max
+		}
+
+		public RectTransform bgRT;
+
+		private EEnd _end;
+
+		public RectTransform cachedRT
+		{
+			private set;
+			get;
+		}
+
+		private GraphScrollBar _graphScrollBar;
+
+		private void Awake( )
+		{
+			cachedRT = GetComponent<RectTransform>( );
+		}
+
+		public void Init( GraphScrollBar gsb, EEnd ee)
+		{
+			_graphScrollBar = gsb;
+			_end = ee;
+
+			gameObject.name = "ScrollEnd_"+ ee.ToString( );
+
+			cachedRT.SetParent( _graphScrollBar.cachedRT );
+			cachedRT.localScale = new Vector3( 1f, 1f, 1f );
+			SetPos( );
+		}
+
+		private void SetPos()
+		{
+			float height = _graphScrollBar.cachedRT.rect.height;
+			switch (_end)
+			{
+				case EEnd.Min:
+					{
+						cachedRT.anchorMin = new Vector2( 0f, 0.5f );
+						cachedRT.anchorMax = new Vector2( 0f, 0.5f );
+						cachedRT.pivot = new Vector2( 0f, 0.5f );
+
+						bgRT.localRotation = Quaternion.Euler( 0f, 0f, 180f );
+						break;
+					}
+				case EEnd.Max:
+					{
+						cachedRT.anchorMin = new Vector2( 1f, 0.5f );
+						cachedRT.anchorMax = new Vector2( 1f, 0.5f );
+						cachedRT.pivot = new Vector2( 1f, 0.5f );
+
+						bgRT.localRotation = Quaternion.Euler( 0f, 0f, 0f );
+						break;
+					}
+			}
+			cachedRT.anchoredPosition = Vector2.zero;
+			cachedRT.sizeDelta = new Vector2( 0.5f * height, height );
+		}
+	}
+
+}
