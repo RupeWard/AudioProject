1b51733db97c22d0acf71a66c078d95caf858827
RupeWard <rupe@rupertward.co.uk>
2017-05-01 17:52:45 +0100

    Initial commit - core files, untested

---
+++ AudioProjectUnity/Assets/Scripts/Scenes/SceneControllerTestScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControllerTestScene : SceneController_Base
+{
+	override public SceneManager.EScene Scene( )
+	{
+		return SceneManager.EScene.TestScene;
+	}
+
+	static private readonly bool DEBUG_LOCAL = false;
+
+
+	// Use this for initialization
+	void Start( )
+	{
+	}
+
+	protected override void PostAwake( )
+	{
+	}
+
+}
