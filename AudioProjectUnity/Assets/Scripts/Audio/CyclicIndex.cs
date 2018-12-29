7e02f9b7b35038f5c46f9a96d0031ab1480231d0
RupeWard <rupe@rupertward.co.uk>
2018-12-29 16:31:59 +0000

    Untested ringbuffer class

---
+++ AudioProjectUnity/Assets/Scripts/Audio/CyclicIndex.cs
@@ -0,0 +1,103 @@
+﻿using RJWS.Core.DebugDescribable;
+using UnityEngine;
+
+namespace RJWS.Core.Data
+{
+	public class CyclicIndex : RJWS.Core.DebugDescribable.IDebugDescribable
+	{
+		private int _capacity;
+
+		private int _value;
+		public int Value
+		{
+			get { return _value; }
+			private set
+			{
+				_value = value;
+				ClampValue( );
+			}
+		}
+
+		public CyclicIndex( int cap, int v = 0 )
+		{
+			_capacity = cap;
+			_value = 0;
+		}
+
+		private CyclicIndex(CyclicIndex other)
+		{
+			_capacity = other._capacity;
+			_value = other._value;
+		}
+
+		public CyclicIndex Clone()
+		{
+			CyclicIndex result = new CyclicIndex( this );
+			return result;
+		}
+
+		public CyclicIndex Increment( int i = 1 )
+		{
+			if (i == 0)
+			{
+				Debug.LogErrorFormat( "CyclicIndex.Increment called with 0" );
+			}
+			Value += i;
+			return this;
+		}
+
+		public CyclicIndex Decrement( int i = 1 )
+		{
+			if (i == 0)
+			{
+				Debug.LogErrorFormat( "CyclicIndex.Decrement called with 0" );
+			}
+			Value -= i;
+			return this;
+		}
+
+		private void ClampValue( )
+		{
+			while (_value >= _capacity)
+			{
+				_value -= _capacity;
+			}
+			while (_value < 0)
+			{
+				_value += _capacity;
+			}
+		}
+
+		public int DistanceAfter(CyclicIndex other)
+		{
+			return Distance( this, other );
+		}
+
+		public int DistanceBefore(CyclicIndex other)
+		{
+			return Distance( other, this );
+		}
+
+		public static int Distance( CyclicIndex first, CyclicIndex second )
+		{
+			if (first._capacity != second._capacity)
+			{
+				throw new System.Exception( "Can't find Distance when CyclicIndex's have different capacities: eg " + first.DebugDescribe( ) + " & " + second.DebugDescribe( ) );
+			}
+			int v = second.Value;
+			while (v < first.Value)
+			{
+				v += second._capacity;
+			}
+
+			return v - first.Value;
+		}
+
+		void IDebugDescribable.DebugDescribe( System.Text.StringBuilder sb )
+		{
+			sb.Append( _value ).Append( "(" ).Append( _capacity ).Append( ")" );
+		}
+	}
+
+
+}
---
+++ AudioProjectUnity/Assets/Scripts/Audio/RingBuffer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RJWS.Core.Data
+{
+	public class RingBuffer
+	{
+		private int _capacity;
+		public int Capacity
+		{
+			get { return _capacity; }
+		}
+
+		private double[] _buffer;
+
+		private CyclicIndex _first;
+		private CyclicIndex _last;
+
+		public RingBuffer(int c)
+		{
+			_capacity = c;
+			_buffer = new double[_capacity];
+			_first = new CyclicIndex( _capacity );
+			_last = new CyclicIndex( _capacity );
+		}
+
+		public int Size
+		{ 
+			get { return _first.DistanceAfter(_last);  } 
+		}
+
+		public bool IsEmpty
+		{
+			get { return Size == 0; } 
+		}
+
+		public bool IsFull
+		{
+			get { return Size >= _capacity; } 
+		}
+
+		public void Enqueue(double d)
+		{
+			if (IsFull)
+			{
+				throw new System.Exception( "Can't Enqueue into full buffer" );
+			}
+			_buffer[_last.Value] = d;
+			_last.Increment( );
+		}
+
+		public double Dequeue()
+		{
+			double result = Peek( );
+			_first.Increment( );
+			return result;
+		}
+
+		public double Peek()
+		{
+			if (IsEmpty)
+			{
+				throw new System.Exception( "Can't Peek at empty buffer" );
+			}
+			return _buffer[_last.Value];
+		}
+	}
+
+}
