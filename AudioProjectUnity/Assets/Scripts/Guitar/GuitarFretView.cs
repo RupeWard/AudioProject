4a896a7a6b31215be7a918134fb6c3fc72a26edb
RupeWard <rupe@rupertward.co.uk>
2018-12-30 19:48:57 +0000

    Fret positions

--- AudioProjectUnity/Assets/Scripts/Guitar/GuitarFretView.cs
+++ AudioProjectUnity/Assets/Scripts/Guitar/GuitarFretView.cs
@@ -12,9 +12,28 @@ namespace RJWS.Audio
 			private set;
 		}
 
+		public Transform fretView;
+
+		private GuitarView _guitarView;
+
 		private void Awake( )
 		{
 			cachedTransform = transform;
+		}
+
+		public void Init(GuitarView gv, int fn)
+		{
+			_guitarView = gv;
+			FretNum = fn;
+
+			fretView.localScale = new Vector3( _guitarView.fretWidth, 0.5f * _guitarView.FretLength, _guitarView.fretWidth );
+			cachedTransform.localPosition = new Vector3( cachedTransform.localPosition.x + _guitarView.FretX( fn ), 0.5f * 0.5f * _guitarView.FretLength + _guitarView.stringSeparation, cachedTransform.localPosition.z );
+		}
+
+		public int FretNum
+		{
+			get;
+			private set;
 		}
 
 	}
--- AudioProjectUnity/Assets/Scripts/Guitar/GuitarView.cs
+++ AudioProjectUnity/Assets/Scripts/Guitar/GuitarView.cs
@@ -6,14 +6,47 @@ namespace RJWS.Audio
 {
 	public class GuitarView : MonoBehaviour
 	{
-		public GuitarStringView guitarStringViewPrefab;
+		public GuitarStringView stringViewPrefab;
+		public GuitarFretView fretViewPrefab;
+
 		public float stringSeparation = 1f;
-		public float stringLength = 15f;
+		public float stringLength = 30f;
 		public float stringWidth = 0.1f;
+		public int numFrets = 10;
+		public float fretWidth = 0.1f;
+
+		public float FretLength
+		{
+			get { return guitarModel.NumStrings * stringSeparation; }
+		}
+		
+		public float BridgeX
+		{
+			get { return -0.5f * stringLength; }
+		}
+
+		public float FretX(int fretNum)
+		{
+			float result = stringLength;
+			if (fretNum > 0)
+			{
+				result /= Mathf.Pow( Core.Audio.AudioConsts.FRET_FACTOR, fretNum );
+			}
+			result = stringLength - result;
+			result = BridgeX + result;
+			return result;
+		}
 
 		private List<GuitarStringView> _stringViews = new List<GuitarStringView>( );
+		private List<GuitarFretView> _fretViews = new List<GuitarFretView>( );
 
 		public Transform cachedTransform
+		{
+			get;
+			private set;
+		}
+
+		public GuitarModel guitarModel
 		{
 			get;
 			private set;
@@ -26,6 +59,8 @@ namespace RJWS.Audio
 
 		public void Init(GuitarModel model)
 		{
+			guitarModel = model;
+
 			if (_stringViews.Count > 0)
 			{
 				for (int i = 0; i < _stringViews.Count; i++)
@@ -34,14 +69,34 @@ namespace RJWS.Audio
 				}
 				_stringViews.Clear( );
 			}
-			for (int i = 0; i < model.NumStrings; i++)
+			for (int i = 0; i < guitarModel.NumStrings; i++)
 			{
-				GuitarStringView gsv = Instantiate( guitarStringViewPrefab );
+				GuitarStringView gsv = Instantiate( stringViewPrefab );
 				gsv.cachedTransform.SetParent( cachedTransform );
 				gsv.gameObject.name = "String_" + i;
 				gsv.cachedTransform.localPosition = new Vector3( 0f, i * stringSeparation, 0f );
 
 				gsv.Init( this, model, i );
+			}
+
+			if (_fretViews.Count > 0)
+			{
+				for (int i = 0; i < _fretViews.Count; i++)
+				{
+					GameObject.Destroy( _fretViews[i].gameObject );
+				}
+				_fretViews.Clear( );
+			}
+
+			for (int i = 0; i <= numFrets; i++)
+			{
+				GuitarFretView gfv = Instantiate( fretViewPrefab );
+				gfv.cachedTransform.SetParent( cachedTransform );
+				gfv.gameObject.name = "Fret_" + i;
+
+				gfv.Init( this, i );
+
+				_fretViews.Add( gfv );
 			}
 		}
 
@@ -49,7 +104,7 @@ namespace RJWS.Audio
 		{
 			get
 			{
-				return new Vector3( stringWidth, stringLength, stringWidth);
+				return new Vector3( stringWidth, 0.5f * stringLength, stringWidth);
 			}
 		}
 	}
