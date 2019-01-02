abd0342dda353c1968d0382762f263230c5c9372
RupeWard <rupe@rupertward.co.uk>
2019-01-02 23:29:54 +0000

    Also persist pluck settings

--- AudioProjectUnity/Assets/Scripts/Guitar/PluckSettings.cs
+++ AudioProjectUnity/Assets/Scripts/Guitar/PluckSettings.cs
@@ -17,12 +17,73 @@ namespace RJWS.Audio
 		public bool useEnter = true;
 		public bool useExit = true;
 
+		private const string PPKEY_SpeedMin = "SpeedMin";
+		private const string PPKEY_SpeedMax = "SpeedMax";
+		private const string PPKEY_VolMin = "VolMin";
+		private const string PPKEY_VolMax = "VolMax";
+		private const string PPKEY_Gamma = "Gamma";
+		private const string PPKEY_UseEnter = "UseEnter";
+		private const string PPKEY_UseExit = "UseExit";
+
+		public void LoadFromPlayerPrefs( )
+		{
+			if (PlayerPrefs.HasKey( PPKEY_SpeedMin ))
+			{
+				speedRange.x = PlayerPrefs.GetFloat( PPKEY_SpeedMin );
+			}
+			else
+			{
+				Debug.LogWarning( "No PP saved" );
+				return;
+			}
+			if (PlayerPrefs.HasKey( PPKEY_SpeedMax ))
+			{
+				speedRange.y = PlayerPrefs.GetFloat( PPKEY_SpeedMax );
+			}
+			if (PlayerPrefs.HasKey( PPKEY_VolMin))
+			{
+				volumeRange.x = PlayerPrefs.GetFloat( PPKEY_VolMin);
+			}
+			if (PlayerPrefs.HasKey( PPKEY_VolMax ))
+			{
+				volumeRange.y = PlayerPrefs.GetFloat( PPKEY_VolMax );
+			}
+			if (PlayerPrefs.HasKey( PPKEY_Gamma))
+			{
+				gamma = PlayerPrefs.GetFloat( PPKEY_Gamma);
+			}
+
+			if (PlayerPrefs.HasKey( PPKEY_UseEnter))
+			{
+				useEnter = (PlayerPrefs.GetInt( PPKEY_UseEnter) == 1);
+			}
+			if (PlayerPrefs.HasKey( PPKEY_UseExit))
+			{
+				useExit= (PlayerPrefs.GetInt( PPKEY_UseExit) == 1);
+			}
+		}
+
+		public void SaveToPlayerPrefs( )
+		{
+			PlayerPrefs.SetFloat( PPKEY_SpeedMin, speedRange.x );
+			PlayerPrefs.SetFloat( PPKEY_SpeedMax, speedRange.y );
+			PlayerPrefs.SetFloat( PPKEY_VolMin, volumeRange.x );
+			PlayerPrefs.SetFloat( PPKEY_VolMax, volumeRange.y );
+			PlayerPrefs.SetFloat( PPKEY_Gamma, gamma );
+		    PlayerPrefs.SetInt( PPKEY_UseEnter, (useEnter)?(1):(0));
+			PlayerPrefs.SetInt( PPKEY_UseExit, (useExit) ? (1) : (0) );
+		}
+
 		static public PluckSettings LoadDefaultsIfNUll(ref PluckSettings gs)
 		{
 			if (gs == null)
 			{
 				gs = Resources.Load( DEFSETTINGSPATH ) as PluckSettings;
 				Debug.Log( "Loaded default pluck settings" );
+
+#if !UNITY_EDITOR
+				gs.LoadFromPlayerPrefs( );
+#endif
 			}
 			return gs;
 		}
